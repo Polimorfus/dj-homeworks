42f41b9607659d9cb33198cbbabc4ed1abd11719
Polimorfus <pguzev@gmail.com>
2023-06-04 20:48:41 +0300

    second commit

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -28,3 +28,32 @@ DATA = {
 #     'ингредиент2': количество2,
 #   }
 # }
+
+def omlet(request):
+    servings = int(request.GET.get("servings"))
+    for x in DATA['omlet']:
+        DATA['omlet'][x] = DATA['omlet'][x]*servings
+    context = {
+        'recipe': DATA['omlet']
+    }
+    return render(request, 'calculator/index.html', context)
+
+
+def pasta(request):
+    servings = int(request.GET.get("servings"))
+    for x in DATA['pasta']:
+        DATA['pasta'][x] = DATA['pasta'][x]*servings
+    context = {
+        'recipe': DATA['pasta']
+    }
+    return render(request, 'calculator/index.html', context)
+
+
+def buter(request):
+    servings = int(request.GET.get("servings"))
+    for x in DATA['buter']:
+        DATA['buter'][x] = DATA['buter'][x]*servings
+    context = {
+        'recipe': DATA['buter']
+    }
+    return render(request, 'calculator/index.html', context)

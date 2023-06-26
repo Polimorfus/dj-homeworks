d0dba62590e7d838dcbb7de4e65073b39857bfee
Polimorfus <pguzev@gmail.com>
2023-06-26 20:02:56 +0300

    ninth commit

--- 3.3-permissions/api_with_restrictions/advertisements/filters.py
+++ 3.3-permissions/api_with_restrictions/advertisements/filters.py
@@ -1,3 +1,4 @@
+import django_filters
 from django_filters import rest_framework as filters
 
 from advertisements.models import Advertisement
@@ -5,8 +6,8 @@ from advertisements.models import Advertisement
 
 class AdvertisementFilter(filters.FilterSet):
     """Фильтры для объявлений."""
-
-    # TODO: задайте требуемые фильтры
+    created_at = django_filters.DateFromToRangeFilter()
 
     class Meta:
         model = Advertisement
+        fields = ['created_at', 'creator', 'status']
--- 3.3-permissions/api_with_restrictions/advertisements/serializers.py
+++ 3.3-permissions/api_with_restrictions/advertisements/serializers.py
@@ -24,22 +24,24 @@ class AdvertisementSerializer(serializers.ModelSerializer):
         model = Advertisement
         fields = ('id', 'title', 'description', 'creator',
                   'status', 'created_at', )
+        read_only_fields = ['creator', ]
 
     def create(self, validated_data):
         """Метод для создания"""
 
-        # Простановка значения поля создатель по-умолчанию.
-        # Текущий пользователь является создателем объявления
-        # изменить или переопределить его через API нельзя.
-        # обратите внимание на `context` – он выставляется автоматически
-        # через методы ViewSet.
-        # само поле при этом объявляется как `read_only=True`
         validated_data["creator"] = self.context["request"].user
         return super().create(validated_data)
 
     def validate(self, data):
         """Метод для валидации. Вызывается при создании и обновлении."""
-
-        # TODO: добавьте требуемую валидацию
-
-        return data
+        if self.context['request'].method == 'POST':
+            if Advertisement.objects.filter(creator_id=self.context['request'].user, status='OPEN').count() <= 10:
+                return data
+            else:
+                raise serializers.ValidationError('Sorry, User has too many notes with status OPEN!')
+        if self.context['request'].method == 'PATCH' and Advertisement.objects.filter(status='OPEN').count() >= 1:
+            if Advertisement.objects.get(id=self.context['request'].parser_context['kwargs']['pk']).status != data['status']\
+                    and Advertisement.objects.filter(creator_id=self.context['request'].user, status='OPEN').count() <= 10:
+                return data
+            else:
+                raise serializers.ValidationError('Sorry, Error is occurred!')
